ebe88a2a8fd7433eac3d313d6e5e2df6f3f35c6a
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-08 15:56:45 -0500

    Modificando panel admin [nombre, busqueda]

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -3,6 +3,10 @@ from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
 
-admin.site.register(Clientes)
+class Clientes_admin(admin.ModelAdmin):
+  list_display=("nombre", "direccion", "telefono")
+  search_fields=("nombre", "telefono")
+
+admin.site.register(Clientes, Clientes_admin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
