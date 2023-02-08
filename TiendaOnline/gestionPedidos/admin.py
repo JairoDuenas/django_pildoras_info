f5f598540ad7086b4cb84862bb1661d549efe5e4
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-08 16:17:21 -0500

    Agregando filtros y cambio de idioma[panel-admin]

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -7,6 +7,14 @@ class Clientes_admin(admin.ModelAdmin):
   list_display=("nombre", "direccion", "telefono")
   search_fields=("nombre", "telefono")
 
+class Articulos_admin(admin.ModelAdmin):
+  list_filter=("seccion",)
+
+class Pedidos_admin(admin.ModelAdmin):
+  list_display=("numero", "fecha", "entregado")
+  list_filter=("fecha",)
+  date_hierarchy="fecha"
+
 admin.site.register(Clientes, Clientes_admin)
-admin.site.register(Articulos)
-admin.site.register(Pedidos)
+admin.site.register(Articulos, Articulos_admin)
+admin.site.register(Pedidos, Pedidos_admin)
