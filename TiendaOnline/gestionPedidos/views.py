ea8154a4b040418a58b31b9829424054fe83d49f
JairoDuenas <jairoduenas.ing@gmail.com>
2023-02-09 08:15:30 -0500

    Crear formulario, envío datos al servidor[objetos HttpRequest]

--- TiendaOnline/gestionPedidos/views.py
+++ TiendaOnline/gestionPedidos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def busqueda_productos(request):
+  return render(request, "busqueda_productos.html", {})
+
+def buscar(request):
+  mensaje=f"Artículo buscado: {request.GET['prod']}"
+  return HttpResponse(mensaje)
